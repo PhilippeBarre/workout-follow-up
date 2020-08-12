a8ae9951d7ae4b78b710ad1a931e52c79b38cc20
Philippe BARRE <philippe.barre.57@gmail.com>
2020-08-13 01:45:36 +0200

    WIP: test method firebase

--- src/app/components/programs/programs.component.ts
+++ src/app/components/programs/programs.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-programs',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProgramsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private afAuth: AngularFireAuth, private angularFirestore: AngularFirestore) { }
 
   ngOnInit() {
+    console.log(this.afAuth.auth.currentUser.uid);
+
+//   var setWithMerge = cityRef.set({
+//       capitssal: true
+//   }, { merge: true }).then(function() {
+//     console.log("Document successfully written!");
+// });
+
+    this.angularFirestore.collection("users").doc(this.afAuth.auth.currentUser.uid).get().subscribe(x => {
+      console.log(x.data());
+    });
   }
 
 }
